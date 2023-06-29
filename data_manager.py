7c408f35c549bcfd0c98431b528f115c18edd5cf
zotya <zoltankiss9210@gmail.com>
2023-06-29 17:57:11 +0200

    refactored

--- data_manager.py
+++ data_manager.py
@@ -2,7 +2,6 @@ import os
 import psycopg2
 import psycopg2.extras
 
-
 def establish_connection(connection_data=None):
     """
     Create a database connection based on the :connection_data: parameter
@@ -12,11 +11,10 @@ def establish_connection(connection_data=None):
     if connection_data is None:
         connection_data = get_connection_data()
     try:
-        # connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
-        #                                                              connection_data['user'],
-        #                                                              connection_data['host'],
-        #                                                              connection_data['password'])
-        connect_str = os.environ.get('DATABASE_URL')
+        connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
+                                                                     connection_data['user'],
+                                                                     connection_data['host'],
+                                                                     connection_data['password'])
         conn = psycopg2.connect(connect_str)
         conn.autocommit = True
     except psycopg2.DatabaseError as e:
